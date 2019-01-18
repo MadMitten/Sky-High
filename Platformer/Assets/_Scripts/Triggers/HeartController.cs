0e37f4f478e47e256e47bb834076fd653e631e9f
ThomasJ400 <35486947+ThomasJ400@users.noreply.github.com>
2019-01-18 23:19:04 +0000

    workable
    workable project commit

---
+++ Platformer/Assets/_Scripts/Triggers/HeartController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartController : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameBehaviour.instance.addLife();
+        Destroy(gameObject);
+    }
+}
